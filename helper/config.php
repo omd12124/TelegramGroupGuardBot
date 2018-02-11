cde09b73d899829f213812b1798b11d124c74809
omd12124 <omid.ghollasi@gmail.com>
2018-02-11 17:41:37 +0330

    Delete config.php

--- helper/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-error_reporting(0);
-$host="localhost";
-$db_user="pingfire_omd123";
-$db_password="Simaran12124@1";
-$database="pingfire_weatherbot";
-$port="";
-$socket="";
-$DB_CONNECTING=new mysqli($host, $db_user, $db_password, $database);
